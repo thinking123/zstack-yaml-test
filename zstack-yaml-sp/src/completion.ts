82e298997258eb2e9a42d8b16cdc0809d048c62b
tking <1294873004@qq.com>
2021-04-14 16:32:22 +0800

    privider

--- zstack-yaml-sp/src/completion.ts
+++ zstack-yaml-sp/src/completion.ts
@@ -6,20 +6,38 @@ import { resources } from './zstack/constants';
 
 
 
-export const providerForResource = vscode.languages.registerCompletionItemProvider('yaml', {
+export const providerForResource = vscode.languages.registerCompletionItemProvider('yaml-injection"', {
 
   provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
 
 
     const resourceCompletions = resources.map(resource => {
-      const zstackResourceCompletion = new vscode.CompletionItem(resource);
+      const zstackResourceCompletion = new vscode.CompletionItem("Fsdfsdfsf");
       zstackResourceCompletion.kind = vscode.CompletionItemKind.Class;
 
       return zstackResourceCompletion;
     });
 
+    debugger
+    console.log('resourceCompletions', resourceCompletions)
     return [
       ...resourceCompletions
     ];
   }
 });
+
+
+
+export const providerForVarible = vscode.languages.registerCompletionItemProvider('yaml', {
+
+  provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+
+
+    const zstackResourceCompletion = new vscode.CompletionItem("Fsdfsdfsf");
+    zstackResourceCompletion.kind = vscode.CompletionItemKind.Class;
+
+    return [
+      zstackResourceCompletion
+    ];
+  }
+}, '(', ')');
