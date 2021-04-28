85c378ea1efb9512f25c5afe2f0903e5aed35470
tking <1294873004@qq.com>
2021-04-28 20:12:21 +0800

    fix: dump yaml

--- zstack-yaml-acorn/src/index.ts
+++ zstack-yaml-acorn/src/index.ts
@@ -1,21 +1,58 @@
 
+import fs from 'fs'
+import path from 'path'
+import yaml from 'js-yaml'
+import { CachedInputFileSystem, ResolverFactory } from 'enhanced-resolve'
 import { jsonToAst } from './ast'
 import { print } from './print'
+import { Logger } from './tsToyaml/logger'
 
-
+const logger = Logger.logger()
 const transform = (json: Object, transformKey: string) => {
 
-  // const transformJson = (json as any)?.[transformKey]
   const ast = jsonToAst(json)
 
   const transformAst = ast?.find(({ name }) => name === transformKey)
   const str = print(transformAst)
   const fun = new Function(str)
 
-  return fun
+  return fun()
 }
 
 const dumpYaml = (yamlFilePath: string, yamlTag: string) => {
+  // const path 
+  // ts.resolveModuleName()
+
+  const myResolver = ResolverFactory.createResolver({
+    alias: {
+      '@test': path.resolve(process.cwd(), 'test')
+    },
+    fileSystem: new CachedInputFileSystem(fs, 4000),
+    extensions: [".yaml",]
+  });
+
+  // resolve a file with the new resolver
+  const context = {};
+  const resolveContext = {};
+  const lookupStartPath = __dirname;
+  const request = yamlFilePath;
+  myResolver.resolve({}, lookupStartPath, request, resolveContext, (
+    err /*Error*/,
+    filepath /*string*/
+  ) => {
+    // Do something with the path
+    if (err || filepath === false) {
+      logger.log(`[dumpYaml]: resolve file failed = ${err} , path = ${yamlFilePath}`)
+      return
+    }
+
+    const fc = fs.readFileSync(filepath, { encoding: "utf-8" })
+
+
+    const json = yaml.load(fc)
+
+    return transform(json, yamlTag)
+  });
 }
 export {
   transform,
