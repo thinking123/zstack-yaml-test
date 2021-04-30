d068778695b648f8bf020d3034be467221b6b51c
tking <1294873004@qq.com>
2021-04-30 18:03:43 +0800

    update bin and version: 1.0.4

--- zstack-yaml-acorn/src/test.ts
+++ zstack-yaml-acorn/src/test.ts
@@ -3,12 +3,12 @@ import path from 'path'
 import yaml from 'js-yaml'
 
 import circularJson from 'circular-json'
-import { transform } from '.'
+// import { transform } from '.'
 
 const yp = path.join(process.cwd(), 'test/assets/ipsec-connection.yaml')
 
 const fc = fs.readFileSync(yp, { encoding: "utf-8" })
 const json = yaml.load(fc)
 
-const res = transform(json, 'ipsecCreate')
-console.log('res', res)
+// const res = transform(json, 'ipsecCreate')
+// console.log('res', res)
